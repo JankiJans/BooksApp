f344e0189dc2da3646f679a69ee9ba31176a781c
Jan Kędzierski <jkedzierski087@gmail.com>
2023-01-19 15:19:31 +0100

    small fix 2 (still not working)

--- src/js/script.js
+++ src/js/script.js
@@ -29,25 +29,27 @@ const favoriteBooks = [];
 function initActions () {
 
   const booksImage = document.querySelectorAll('.book__image');
+  
 
-  for(let book of booksImage){
+  for(let image of booksImage){
 
-    book.addEventListener('dbclick', function(event){ 
-    event.preventDefault();
+    image.addEventListener('dbclick', function(event){ 
+      event.preventDefault();
 
-      book.classList.toggle('favorite');
+      image.classList.toggle('favorite');
 
-      const bookId = book.getAttribute('data-id'); 
+      const bookId = image.getAttribute('data-id'); 
 
-        if (book.classList.contains('favorite')) { //sprawdzamy czy klasa favorite jest dodana do elementu
-          favoriteBooks.push(bookId);  //dodajemy do tablicy id książki     
-        } else { //jeśli nie ma klasy favorite to usuwamy z tablicy
-          favoriteBooks.splice(favoriteBooks.indexOf(bookId), 1); //usuwanie z tablicy
-        }
-
-        console.log(favoriteBooks);
+      if (image.classList.contains('favorite')) { //sprawdzamy czy klasa favorite jest dodana do elementu
+        favoriteBooks.push(bookId);  //dodajemy do tablicy id książki 
+        
+      } else { //jeśli nie ma klasy favorite to usuwamy z tablicy
+        favoriteBooks.splice(favoriteBooks.indexOf(bookId), 1); //usuwanie z tablicy
+      }
 
     });
+
+    console.log(favoriteBooks);
 
   } 
 }
