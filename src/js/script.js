931a89ad71cc1b8bcd12e2aea83d12f969b05908
Jan Kędzierski <jkedzierski087@gmail.com>
2023-01-19 14:26:27 +0100

    add initActions (not working)

--- src/js/script.js
+++ src/js/script.js
@@ -11,16 +11,48 @@ console.log(booksList);
 //Dodaj funckję render, wewnątrz niej przejdź po każdym elemencie z dataSource.books
 function render() {
 
-    for (let book of dataSource.books) {
-        const generatedHTML = template(book);
-        booksList.innerHTML += generatedHTML;     
+  for (let book of dataSource.books) {
+    const generatedHTML = template(book);
+    booksList.innerHTML += generatedHTML;     
 
-        console.log(generatedHTML);
-    }  
+    console.log(generatedHTML);
+  }  
 }
 render();
 
+/* DODOWANIE KSIĄŻEJ DO ULUBIONYCH */
 
+//tablica z ulubionymi książkami
+
+const favoriteBooks = [];
+
+function initActions () {
+
+  const booksImage = document.querySelectorAll('.book__image');
+
+  for(let book of booksImage){
+
+    booksImage.addeventListener('dbclick', function(event){ 
+    event.preventDefault();
+
+      book.classList.toggle('favorite');
+
+      const bookId = book.getAttribute('data-id'); 
+
+        if (book.classList.contains('favorite')) { //sprawdzamy czy klasa favorite jest dodana do elementu
+          favoriteBooks.push(bookId);  //dodajemy do tablicy id książki     
+        } else { //jeśli nie ma klasy favorite to usuwamy z tablicy
+          favoriteBooks.splice(favoriteBooks.indexOf(bookId), 1); //usuwanie z tablicy
+        }
+
+        console.log(favoriteBooks);
+
+    });
+
+  } 
+}
+
+initActions();
 
 
 
